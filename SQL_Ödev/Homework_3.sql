a6b1683bf7bc361d30886ce8295ceb1dd6ba911e
Mehmet Ünlütürk <90462313+Mehmet-Unluturk@users.noreply.github.com>
2022-12-20 16:09:05 +0300

    Delete SQL_Ödev directory

--- SQL_Ödev/Homework_3.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM country where country like 'A%a';
-SELECT * FROM country;
-SELECT country  FROM country WHERE country LIKE'-----%n';
-SELECT title FROM film LIKE '%t%t%t%t';
-SELECT *FROM film WHERE title LIKE 'C%' AND length>90 AND rental_rate=2.99;
